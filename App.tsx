d574227e07d5a1dd51fa0879742e08df031068b2
riansco14 <rian.rabelo@ufrpe.br>
2022-06-03 22:19:56 -0300

    Rotas de Telas

--- App.tsx
+++ App.tsx
@@ -1,21 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useFonts, Jost_400Regular, Jost_600SemiBold } from '@expo-google-fonts/jost';
 import AppLoading from 'expo-app-loading';
 import Routes from './src/routes';
-import { PlantSelect } from './src/pages/PlantSelect';
+
+import * as  Notifications from 'expo-notifications'
+import { PlantProps } from './src/libs/storage';
 
 export default function App() {
     const [fontsLoaded] = useFonts({
         Jost_400Regular, Jost_600SemiBold
     });
+
+    useEffect(() => {
+        async function notifications() {
+
+            await Notifications.cancelAllScheduledNotificationsAsync()
+
+            const data = await Notifications.getAllScheduledNotificationsAsync()
+            console.log("###### NOTIFICAÇÕES AGENDADAS ######");
+            console.log(data);
+            
+        }
+        notifications()
+    },[])
     
     if (!fontsLoaded) {
         return <AppLoading/>
     }
 
     return (
-
-        <PlantSelect />
+        <Routes/>
+        //<PlantSelect />
 
     )
 }
